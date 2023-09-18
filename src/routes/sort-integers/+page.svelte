5910ac62b557d32aa9958d2a716955ef63d220ee
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-09-18 23:53:12 +0200

    Delete and continue button
    No algorithms yet

--- src/routes/sort-integers/+page.svelte
+++ src/routes/sort-integers/+page.svelte
@@ -4,6 +4,7 @@
 
 	import { scale } from 'svelte/transition';
 	import { flip } from 'svelte/animate';
+	import { PlaySolid, PlusSolid, TrashBinSolid } from 'flowbite-svelte-icons';
 
 	let currentStep = 1;
 	let steps = ['Zahlen eingeben', 'Algorithmus auswählen', 'Sortieren'].map(
@@ -51,7 +52,27 @@
 	<StepIndicator class="mt-4 mb-8" {currentStep} {steps} glow />
 
 	{#if currentStep == 1}
-		<Gallery class="gap-4 md:grid-cols-4 grid-cols-2 mx-4 mt-4">
+		<Gallery class="gap-4 md:grid-cols-4 grid-cols-2 mx-4 my-4">
+			<Card transition={scale} size="lg" class="col-span-1">
+				<Button
+					class="mt-2 text-md"
+					color="red"
+					on:click={() => (numbers = [])}
+					disabled={numbers.length == 0}
+				>
+					<TrashBinSolid class="mr-2" />
+					Löschen</Button
+				>
+				<Button
+					class="mt-2 text-md"
+					color="green"
+					on:click={() => currentStep++}
+					disabled={numbers.length < 5}
+				>
+					<PlaySolid class="mr-2" />
+					Fortfahren</Button
+				>
+			</Card>
 			<Card transition={scale} size="lg" class="col-span-2">
 				<Toast class="mx-0" bind:open={error}>Gib bitte eine Zahl zwischen 0 und 999 ein.</Toast>
 				<Gallery class="gap-6 grid-cols-2">
@@ -59,7 +80,10 @@
 						<Label for="num-input">Zahl eingeben</Label>
 						<Input id="num-input" class="text-3xl" bind:value placeholder={funnyNumber} />
 					</div>
-					<Button class="mt-2 text-xl" on:click={addNumber}>Hinzufügen</Button>
+					<Button class="mt-2 text-xl" on:click={addNumber}>
+						<PlusSolid class="mr-2" />
+						Hinzufügen
+					</Button>
 				</Gallery>
 			</Card>
 			{#each numbers as number, index (number.id)}
@@ -72,7 +96,7 @@
 						}}
 					>
 						<h5
-							class="mb-2 text-8xl font-light mx-auto tracking-tight text-gray-900 dark:text-white"
+							class="px-0 mb-2 text-8xl font-light mx-auto tracking-tight text-gray-900 dark:text-white"
 						>
 							{number.value}
 						</h5>
