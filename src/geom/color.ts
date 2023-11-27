63de104f1b5cdf9d43d435cf335b0ef3215e07bf
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-11-27 17:10:00 +0100

    Remove unnecessary global set

--- src/geom/color.ts
+++ src/geom/color.ts
@@ -92,8 +92,6 @@ export class Color {
 	}
 }
 
-window.Myc = Color;
-
 export const rgbComponentSchema = z.enum(['r', 'g', 'b']);
 export type RgbComponent = z.infer<typeof rgbComponentSchema>;
 export const hsvComponentSchema = z.enum(['h', 's', 'v']);
