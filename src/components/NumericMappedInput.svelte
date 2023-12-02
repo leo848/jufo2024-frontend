142a6eef6cad6d61723352f50cc9c310d5606573
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-12-02 21:40:06 +0100

    Add rounding to numeric mapped input

--- src/components/NumericMappedInput.svelte
+++ src/components/NumericMappedInput.svelte
@@ -34,7 +34,7 @@
 </script>
 
 <div class="flex flex-row text-xl">
-	<div class="w-6 flex flex-col justify-stretch">
+	<div class="w-6 flex flex-col justify-stretch rounded-l-md overflow-hidden ">
 		<button
 			class="w-6 bg-gray-600 hover:bg-gray-500 transition-all disabled:bg-gray-700 h-full text-sm text-center"
 			disabled={displayValue > max - step}
@@ -51,7 +51,7 @@
 		</button>
 	</div>
 	<input
-		class="w-14 px-2 text-xl text-white bg-gray-700 border-none text-center"
+		class="w-14 px-2 text-xl text-white bg-gray-700 border-none rounded-r-md overflow-hidden text-center border-l-4 border-gray-500"
 		{max}
 		{min}
 		step="1"
