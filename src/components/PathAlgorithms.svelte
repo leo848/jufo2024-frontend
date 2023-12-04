73ebba0feb6b0e4f3380f0138d519d683d651c6a
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-12-04 23:53:26 +0100

    Foundations for improvement

--- src/components/PathAlgorithms.svelte
+++ src/components/PathAlgorithms.svelte
@@ -15,73 +15,93 @@
 	let progress = { ongoing: false, value: 0 as null | number };
 	let path: number[][] | null = null;
 
-	const constructionItems: {
-		name: string;
-		description: string;
-		icon: ComponentType;
-		index: number;
-		expectedTime?: (n: number) => null | number;
-		send: (() => void) | null;
-	}[] = (
-		[
-			{
-				name: 'Manuell',
-				description: 'Manuelle Auswahl der Punkte in einer Reihenfolge',
-				method: null,
-				expectedTime: () => null,
-				icon: Icon.AnnotationOutline
-			},
-			{
-				name: 'Greedy',
-				description: 'Greedy-Algorithmus',
-				method: 'greedy',
-				icon: Icon.DollarOutline
-			},
-			{
-				name: 'Nearest Neighbor',
-				description: 'Nächster Nachbar',
-				method: 'nearestNeighbor',
-				expectedTime: (n: number) => Math.min(n / 2, 5),
-				icon: Icon.PhoneOutline
-			},
-			{
-				name: 'Brute Force',
-				description: 'Teste alle möglichen Kombinationen',
-				method: 'bruteForce',
-				expectedTime: (n: number) => factorial(n) / 360,
-				icon: Icon.HourglassOutline
-			},
-			{
-				name: 'Optimal (Concorde)',
-				description: 'Finde die optimale Lösung mittels des externen Tools Concorde',
-				method: null,
-				icon: Icon.WandMagicSparklesOutline
-			}
-		] as const
-	).map((e, i) => {
-		const payload =
-			e.method &&
-			(() =>
-				({
-					type: 'action',
-					action: {
-						type: 'createPath',
-						method: { type: e.method },
-						dimensions,
-						values: points
-					}
-				} as const));
-		const send = payload && (() => sendWebsocket(payload()));
-		return Object.assign({}, e, { index: i, send });
-	});
+	type ActionKind = 'construction' | 'improvement';
+	type ForAction<T> = Record<ActionKind, T>;
+	const name = { construction: 'Konstruktion', improvement: 'Verbesserung' };
 
-	let selectedConstructionItem: null | number = null;
-	let displayConstructionItems = constructionItems;
+	let currentAction: ActionKind = 'construction';
+	$: currentAction = path === null ? 'construction' : 'improvement';
+
+	const items: ForAction<
+		{
+			name: string;
+			description: string;
+			icon: ComponentType;
+			index: number;
+			expectedTime?: (n: number) => null | number;
+			send: (() => void) | null;
+		}[]
+	> = {
+		construction: (
+			[
+				{
+					name: 'Manuell',
+					description: 'Manuelle Auswahl der Punkte in einer Reihenfolge',
+					method: null,
+					expectedTime: () => null,
+					icon: Icon.AnnotationOutline
+				},
+				{
+					name: 'Greedy',
+					description: 'Greedy-Algorithmus',
+					method: 'greedy',
+					icon: Icon.DollarOutline
+				},
+				{
+					name: 'Nearest Neighbor',
+					description: 'Nächster Nachbar',
+					method: 'nearestNeighbor',
+					expectedTime: (n: number) => Math.min(n / 2, 5),
+					icon: Icon.PhoneOutline
+				},
+				{
+					name: 'Brute Force',
+					description: 'Teste alle möglichen Kombinationen',
+					method: 'bruteForce',
+					expectedTime: (n: number) => factorial(n) / 360,
+					icon: Icon.HourglassOutline
+				},
+				{
+					name: 'Optimal (Concorde)',
+					description: 'Finde die optimale Lösung mittels des externen Tools Concorde',
+					method: null,
+					icon: Icon.WandMagicSparklesOutline
+				}
+			] as const
+		).map((e, i) => {
+			const payload =
+				e.method &&
+				(() =>
+					({
+						type: 'action',
+						action: {
+							type: 'createPath',
+							method: { type: e.method },
+							dimensions,
+							values: points
+						}
+					} as const));
+			const send = payload && (() => sendWebsocket(payload()));
+			return Object.assign({}, e, { index: i, send });
+		}),
+		improvement: []
+	};
+
+	let selectedItem: ForAction<null | number> = { construction: null, improvement: null };
+	$: selectedItemAction = selectedItem[currentAction];
+	let displayItems = items;
 	$: {
-		let find = constructionItems.find((e) => e.index === selectedConstructionItem);
-		displayConstructionItems = find === undefined ? constructionItems : [find];
+		displayItems = Object.fromEntries(
+			Object.entries(items)
+				.map(([key, arr]) => [
+					key,
+					arr,
+					arr.find((e) => e.index === selectedItem[key as ActionKind])
+				])
+				.map(([key, arr, elem]) => [key, elem === undefined ? arr : [elem]])
+		);
 	}
-	$: constructionOpen = selectedConstructionItem !== null;
+	$: open = (kind: ActionKind) => selectedItem[kind] !== null;
 
 	let callbackId = registerCallback(serverOutputPathCreation, (pc) => {
 		if (pc.donePath) {
@@ -98,33 +118,35 @@
 
 <Card class="rounded-xl col-span-12 md:col-span-6 lg:col-span-7 xl:col-span-4 max-w-none">
 	<p class="text-2xl xl:text-3xl dark:text-white bg-gray-700 p-4 -m-6 rounded-t-xl mb-4">
-		Konstruktion
+		{name[currentAction]}
 	</p>
 	<div class="text-xl grow flex flex-col">
-		{#each displayConstructionItems as { name, icon, index } (index)}
+		{#each displayItems[currentAction] as item (item.index)}
 			<button
 				animate:flip={{ duration: 200 }}
 				transition:scale
 				class="transition"
-				on:click={() => (selectedConstructionItem = constructionOpen ? null : index)}
+				on:click={() => (selectedItem[currentAction] = open(currentAction) ? null : item.index)}
 				disabled={progress.ongoing}
 			>
-				<div class="bg-gray-700 hover:bg-gray-600 transition-all p-4 mb-4 rounded-xl border border-gray-600">
+				<div
+					class="bg-gray-700 hover:bg-gray-600 transition-all p-4 mb-4 rounded-xl border border-gray-600"
+				>
 					<div class="flex items-center justify-between">
 						<Icon.AngleRightSolid
 							class="transition"
-							style={`transform: rotate(${constructionOpen ? 90 : 0}deg)`}
+							style={`transform: rotate(${open(currentAction) ? 90 : 0}deg)`}
 						/>
 						<span class="font-bold text-2xl">
-							{name}
+							{item.name}
 						</span>
-						<svelte:component this={icon} />
+						<svelte:component this={item.icon} />
 					</div>
 				</div>
 			</button>
 		{/each}
-		{#if selectedConstructionItem !== null}
-			{@const { description, send, expectedTime } = constructionItems[selectedConstructionItem]}
+		{#if selectedItemAction !== null}
+			{@const { description, send, expectedTime } = items[currentAction][selectedItemAction]}
 			<div class="flex flex-col justify-between grow">
 				<div>
 					<div class="rounded-xl" in:scale={{ delay: 150 }}>{description}</div>
@@ -141,7 +163,9 @@
 				</div>
 				{#if send}
 					{#if !progress.ongoing}
-						<button on:click={send} class="rounded-xl text-white bg-gray-700 hover:bg-gray-600 transition-all p-4"
+						<button
+							on:click={send}
+							class="rounded-xl text-white bg-gray-700 hover:bg-gray-600 transition-all p-4"
 							>Ausführen</button
 						>
 					{:else if progress.value === null}
