7684d53c64f06bad28a52e3adfdf8fc93221d76f
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-12-14 09:17:38 +0100

    Titel zu Ansicht des Graphen hinzufügen

--- src/routes/sort-vectors/+page.svelte
+++ src/routes/sort-vectors/+page.svelte
@@ -4,6 +4,7 @@
 	import PathProperties from '../../components/PathProperties.svelte';
 	import PathAlgorithms from '../../components/PathAlgorithms.svelte';
 	import { Card } from 'flowbite-svelte';
+	import * as Icon from 'flowbite-svelte-icons';
 
 	title.set('Vektoren sortieren');
 
@@ -62,7 +63,18 @@
 	<div
 		class="mt-8 grid grid-cols-12 gap-8 auto-cols-max align-stretch justify-stretch justify-items-stretch"
 	>
-		<Card class="rounded-xl col-span-12 xl:col-span-5 max-w-none xl:p-0 mb-0" />
+		<Card class="rounded-xl col-span-12 xl:col-span-5 max-w-none xl:p-0 mb-0" >
+			<div
+				class="text-2xl xl:text-3xl dark:text-white bg-gray-700 p-4 rounded-t-xl flex flex-row justify-between items-center"
+			>
+				<div>Ansicht des Graphen</div>
+				<button
+					class="bg-gray-600 rounded-xl p-2"
+	 				disabled
+					><Icon.CogOutline /></button
+				>
+			</div>
+		</Card>
 
 		<PathProperties {path} {length} />
 
