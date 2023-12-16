1233c7ac8da9043f2e26c6e64927ae8e73e477f8
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-12-16 20:39:27 +0100

    Add map thing
    choosing the provider took longer than expected

---
+++ src/components/OpenLayersMap.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import L from 'leaflet';
+	import 'leaflet/dist/leaflet.css';
+	import 'leaflet/dist/images/marker-icon.png';
+
+	let wrapperDiv: HTMLDivElement;
+	let map: L.map;
+
+	onMount(() => {
+		map = L.map(wrapperDiv, {
+			center: [50.77450,6.08382],
+			zoom: 13,
+		});
+
+		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(map);
+		
+		L.marker([50.77450, 6.08382]).addTo(map)
+			.bindPopup('A pretty CSS popup.<br> Easily customizable.')
+			.openPopup();
+
+		console.log(map);
+	});
+</script>
+
+<div class="h-[420px]" bind:this={wrapperDiv}>
+</div>
--- src/routes/sort-places/+page.svelte
+++ src/routes/sort-places/+page.svelte
@@ -2,6 +2,7 @@
 	import { title } from '../../ui/navbar';
 	import { Card } from 'flowbite-svelte';
 	import * as Icon from 'flowbite-svelte-icons';
+	import OpenLayersMap from '../../components/OpenLayersMap.svelte';
 
 	title.set("Orte sortieren");
 </script>
@@ -10,14 +11,15 @@
 	<div
 		class="mt-8 grid grid-cols-12 gap-8 auto-cols-max align-stretch justify-stretch justify-items-stretch"
 	>
-		<Card class="rounded-xl col-span-12 xl:col-span-5 max-w-none xl:p-0 mb-0">
+		<Card class="rounded-xl overflow-hidden col-span-12 xl:col-span-5 max-w-none xl:p-0 mb-0">
 			<div
 				class="text-2xl xl:text-3xl dark:text-white bg-gray-700 p-4 rounded-t-xl flex flex-row justify-between items-center"
 			>
 				<div>Karte</div>
 				<button class="bg-gray-600 rounded-xl p-2" disabled><Icon.CogOutline /></button>
 			</div>
-			<div class="h-full m-0 min-h-[420px]">
+			<div class="h-full m-0 h-[420px]">
+				<OpenLayersMap />
 			</div>
 		</Card>
 
