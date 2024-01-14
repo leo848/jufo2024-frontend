ae68d11739f857c299a7e082949cddf3e96da312
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2024-01-14 19:07:26 +0100

    Always show amount

--- src/components/PathProperties.svelte
+++ src/components/PathProperties.svelte
@@ -57,27 +57,27 @@
 <Window title="Eigenschaften" xlCol={horizontal ? 6 : 3}>
 	<div class="text-xl tabular-nums">
 		<div class={`grid grid-cols-${horizontal ? 4 : 2} gap-4 m-4`}>
-			{#if path !== null}
-				<div
-					class="col-span-2 rounded-2xl bg-gray-700 py-4 text-gray-400 flex-col flex justify-around items-center"
-				>
-					<div class="text-8xl">{chainLengthDisplay}</div>
-					<div>Kettenlänge</div>
-				</div>
 				<div
 					class="col-span-1 rounded-2xl bg-gray-700 py-4 text-gray-400 flex-col flex justify-around items-center"
 				>
 					<div class="text-5xl">{$displayLength.toFixed(0)}</div>
 					<div>Anzahl</div>
 				</div>
+			{#if path !== null}
 				<div
 					class="col-span-1 rounded-2xl bg-gray-700 py-4 text-gray-400 flex-col flex justify-around items-center"
 				>
 					<div class="text-5xl">{averageDistDisplay}</div>
 					<div>Ø Distanz</div>
 				</div>
+				<div
+					class="col-span-2 rounded-2xl bg-gray-700 py-4 text-gray-400 flex-col flex justify-around items-center"
+				>
+					<div class="text-8xl">{chainLengthDisplay}</div>
+					<div>Kettenlänge</div>
+				</div>
 			{:else}
-				<div class="col-span-2 m-10 p-4 bg-gray-700 rounded">
+				<div class="col-span-2 p-4 bg-gray-700 rounded">
 					{#if blownUp}
 						<div class="text-4xl text-white">Pfad gelöscht</div>
 						<div>
