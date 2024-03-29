ab315807d7e5378e065238edda38139cd5f44169
l blume <leonardo.blume@gymnasium-essen-werden.de>
2024-03-29 22:18:15 +0100

    No adjacency matrix border shifts

--- src/components/AdjacencyMatrix.svelte
+++ src/components/AdjacencyMatrix.svelte
@@ -85,10 +85,8 @@
 				{@const value = vector[index1]}
 				{#if index != index1}
 					<div
-						class="tabular-nums py-2 px-2 border-gray-500 border transition-all"
+						class="tabular-nums py-2 px-2 border-gray-700 border-2 border transition-all"
 						class:border-white={highlightMatrix[index][index1]}
-						class:border-1={!highlightMatrix[index][index1]}
-						class:border-2={highlightMatrix[index][index1]}
 						style={`background-color:${(
 							distColor(value, [min, max]) ?? new RgbColor(0.2, 0.2, 0.2).color()
 						)
