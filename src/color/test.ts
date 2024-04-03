d72d87cb50e66cb779a09fc02e99dfdcec3e3066
l blume <leonardo.blume@gymnasium-essen-werden.de>
2024-04-03 23:31:07 +0200

    Add random colors function

---
+++ src/color/test.ts
@@ -0,0 +1,6 @@
+import type {Color} from "./color";
+import {RgbColor} from "./colorSpaces";
+
+export function randomColors(n: number): Color[] {
+	return new Array(n).fill(0).map(_ => new RgbColor(Math.random(), Math.random(), Math.random()).color());
+}
