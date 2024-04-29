5c84f0bbfb8fd02671ac685f26c9982af730701d
l blume <leonardo.blume@gymnasium-essen-werden.de>
2024-04-29 22:45:55 +0200

    FDGD options light mode

--- src/components/ForceDirectedGraphOptions.svelte
+++ src/components/ForceDirectedGraphOptions.svelte
@@ -16,14 +16,14 @@
 
 <div>
 	<div
-		class="flex flex-row justify-start gap-4 items-center py-2 text-2xl text-white threedoptions-parent p-2 border-transparent m-0"
+		class="flex flex-row justify-start gap-4 items-center py-2 text-2xl dark:text-white text-gray-900 threedoptions-parent p-2 border-transparent m-0"
 		transition:slide={{ axis: 'y' }}
 	>
 		<div
-			class="ball-setting bg-gray-700 rounded-xl p-2 flex flex-row gap-2 overflow-hidden threedoptions-child"
+			class="ball-setting bg-gray-200 dark:bg-gray-700 rounded-xl p-2 flex flex-row gap-2 overflow-hidden threedoptions-child"
 		>
 			<button
-				class="transition bg-gray-600 rounded-lg px-2 flex flex-row items-center gap-2"
+				class="transition dark:bg-gray-600 bg-gray-300 rounded-lg px-2 flex flex-row items-center gap-2"
 				disabled
 			>
 				<span><Icon.RocketSolid /></span>
@@ -38,21 +38,23 @@
 				</div>
 			</button>
 			<button
-				class="transition bg-orange-600 disabled:bg-gray-600 rounded-full p-2"
+				class="transition bg-orange-600 disabled:bg-gray-600 rounded-full p-2 text-white"
 				disabled={options.speed <= 1}
 				on:click={() => ((options.speed -= 1), (inner.speedChange = -1))}
 			>
 				<Icon.MinusSolid />
 			</button>
 			<button
-				class="transition bg-orange-600 disabled:bg-gray-600 rounded-full p-2"
+				class="transition bg-orange-600 disabled:bg-gray-600 rounded-full p-2 text-white"
 				disabled={options.speed >= 5}
 				on:click={() => ((options.speed += 1), (inner.speedChange = 1))}
 			>
 				<Icon.PlusSolid />
 			</button>
 		</div>
-		<button class={`bg-gray-700 rounded-xl p-3`} on:click={actions.freeze}> Einfrieren </button>
+		<button class={`dark:bg-gray-700 bg-gray-200 rounded-xl p-3`} on:click={actions.freeze}>
+			Einfrieren
+		</button>
 	</div>
 </div>
 
