83f98bbc03cc5427714d86fb793eebf694010f3b
l blume <leonardo.blume@gymnasium-essen-werden.de>
2024-05-29 19:33:54 +0200

    Set document title on page title

--- src/ui/navbar.ts
+++ src/ui/navbar.ts
@@ -1,4 +1,10 @@
-import type { ComponentType } from 'svelte';
-import { writable, type Writable } from 'svelte/store';
+import type {ComponentType} from 'svelte';
+import {writable, type Writable} from 'svelte/store';
 
 export const title: Writable<null | string | ComponentType> = writable(null);
+
+title.subscribe(title => {
+	if (typeof title === "string") {
+		document.title = title
+	}
+})
